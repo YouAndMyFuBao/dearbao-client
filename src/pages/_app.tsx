6407d9d43652561546e370d170b21c59f019bc03
Serim Min <97885933+anonymousRecords@users.noreply.github.com>
2024-01-20 22:55:17 +0900

    [FEAT] Layout 컴포넌트 구현 (#6)
    * feat : 글꼴 설정
    
    * style : reset css 적용
    
    * feat : Layout 컴포넌트 구현
    
    * docs : 오타 수정
    
    * docs : PR 템플릿명 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
+import { Layout } from './components/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
@@ -10,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <HydrationBoundary state={pageProps.dehydratedState}>
         <ReactQueryDevtools initialIsOpen={false} />
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </HydrationBoundary>
     </QueryClientProvider>
   );
